156061e212d1d87994348986149b04568c08ca16
taira-taiki <taira-taiki@kcgrp.jp>
2020-11-12 09:10:37 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # weightrec-backend
+
+
+
+## prepare
+```
+go get -u github.com/go-gonic/gin
+```
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+func main() {
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "pong",
+        })
+    })
+
+    r.GET("/user/:name/*action", func(c *gin.Context) {
+        name := c.Param("name")
+        action := c.Param("action")
+        message := name + " is " + action
+        c.String(http.StatusOK, message)
+    })
+    r.Run()
+}
