8e10d39d2494854e990a57a0a16e284c97359068
taira-taiki <taira-taiki@kcgrp.jp>
2020-11-30 08:48:00 +0900

    add struct of user

---
+++ user.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"errors"
+	"time"
+)
+
+// User is user
+type User struct {
+	name                  string
+	weightData            map[recordedDate]float32
+	bodyFatPercentageData map[recordedDate]float32
+}
+
+type recordedDate struct {
+	date time.Time
+}
+
+// CreateUser create user in this app
+func CreateUser(userName string) User {
+	return User{
+		name:                  userName,
+		weightData:            make(map[recordedDate]float32),
+		bodyFatPercentageData: make(map[recordedDate]float32),
+	}
+}
+
+// Name return user name
+func (u *User) Name() string {
+	return u.name
+}
+
+// AddWeight add weight to map
+func (u *User) AddWeight(weight float32, recordedWeightDate time.Time) error {
+	rdate := recordedDate{
+		date: alignDate(recordedWeightDate),
+	}
+	if _, ok := u.weightData[rdate]; ok {
+		return errors.New("Key is exists")
+	}
+
+	u.weightData[rdate] = weight
+	return nil
+}
+
+// GetWeight return user weight at recordedDate
+func (u *User) GetWeight(recordedWeightDate time.Time) (float32, error) {
+	rdate := recordedDate{
+		date: alignDate(recordedWeightDate),
+	}
+
+	if val, ok := u.weightData[rdate]; ok {
+		return val, nil
+	}
+
+	return 0.0, errors.New("Key is not exists")
+}
+
+// GetWeightFromRange return user weight for startDate to endDate
+func (u *User) GetWeightFromRange(startDate time.Time, endDate time.Time) (map[time.Time]float32, error) {
+
+	if startDate.After(endDate) {
+		return nil, errors.New("start date behind end date")
+	}
+
+	s := alignDate(startDate)
+	e := alignDate(endDate)
+
+	r := make(map[time.Time]float32)
+
+	// include end date to returning map
+	for t := s; e.Add(time.Duration(24) * time.Hour).After(t); t = t.Add(time.Duration(24) * time.Hour) {
+		rd := recordedDate{
+			date: t,
+		}
+		if val, ok := u.weightData[rd]; ok {
+			r[t] = val
+		}
+	}
+
+	return r, nil
+}
+
+func alignDate(t time.Time) time.Time {
+	return time.Date(t.Year(),
+		t.Month(),
+		t.Day(),
+		00,
+		00,
+		00,
+		0,
+		time.Local)
+}
---
+++ user_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"log"
+	"testing"
+	"time"
+)
+
+func TestCreateUser(t *testing.T) {
+
+	name := "test-user"
+	u := CreateUser(name)
+	if u.Name() != name {
+		t.Errorf("got: %v\nwant: %v", u.Name(), name)
+	}
+}
+
+func TestGetWeight(t *testing.T) {
+	name := "test-user"
+	u := CreateUser(name)
+
+	var testWeight float32 = 62.3
+	date := time.Date(2020, 11, 22, 23, 59, 59, 0, time.Local)
+	err := u.AddWeight(testWeight, date)
+	if err != nil {
+		t.Errorf("Failed to add weight")
+	}
+
+	weight, err := u.GetWeight(date)
+	if err != nil {
+		t.Errorf("Failed to get weight")
+	}
+
+	if weight != testWeight {
+		t.Errorf("got: %f\nwant: %f", weight, testWeight)
+	}
+
+	testNotExistsDate := time.Date(2020, 11, 29, 23, 59, 59, 0, time.Local)
+	_, err2 := u.GetWeight(testNotExistsDate)
+	if err2 == nil {
+		t.Errorf("Could not get Error")
+	}
+}
+
+func TestGetWeightFromRange(t *testing.T) {
+	name := "test-user"
+	u := CreateUser(name)
+
+	startDay := 22
+	endDay := 26
+
+	start := time.Date(2020, 11, startDay, 22, 21, 00, 0, time.Local)
+	end := time.Date(2020, 11, endDay, 12, 31, 00, 0, time.Local)
+
+	testWeight := make(map[time.Time]float32)
+	testDate := make([]time.Time, endDay-startDay+1)
+
+	for i := 0; i < endDay-startDay+1; i++ {
+		testDate[i] = time.Date(2020, 11, startDay+i, 00, 00, 00, 0, time.Local)
+	}
+
+	testWeight[testDate[0]] = 52.1
+	testWeight[testDate[1]] = 52.1
+	testWeight[testDate[2]] = 53.2
+	testWeight[testDate[3]] = 55.1
+	testWeight[testDate[4]] = 54.3
+
+	for j := 0; j < endDay-startDay+1; j++ {
+		u.AddWeight(testWeight[testDate[j]], testDate[j])
+	}
+
+	result, err := u.GetWeightFromRange(start, end)
+	if err != nil {
+		t.Errorf("Failed to get weight for week")
+	}
+	log.Println(result)
+
+	for _, d := range testDate {
+		if testWeight[d] != result[d] {
+			t.Errorf("got: %f\nwant: %f", result[d], testWeight[d])
+		}
+	}
+
+	_, err2 := u.GetWeightFromRange(end, start)
+	if err2 == nil {
+		t.Errorf("Should get error")
+	}
+
+}
