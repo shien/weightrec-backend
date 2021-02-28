6b766cf73137fff8c367f08f602c2d6687b2a0ff
taira-taiki <taira-taiki@kcgrp.jp>
2021-02-28 22:50:53 +0900

    fix

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -28,12 +28,12 @@ func Init() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	log.Println("Shutting down server...")
+	log.Println("Shutting down server now...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Fatal: Server forced to shutdown ", err)
+		log.Fatal("Fatal: Forced to shutdown ", err)
 	}
 
 	log.Println("Server exiting")
